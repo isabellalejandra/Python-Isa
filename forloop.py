916b19f7cabd4250a287edbd5db2f29d524ad356
Isabella Barrera <120070445+isabellalejandra@users.noreply.github.com>
2023-12-08 12:49:33 -0800

    using for loop in python

---
+++ forloop.py
@@ -0,0 +1,19 @@
+# #!/usr/bin/env python3 
+
+#using Python Intersctive shell
+python 3 
+
+#entering a command that prints a welcome statement 
+print("Welcome to Isabella's Python script")
+
+# this is an example of using a for loop in Python interactive shell 
+val = 2
+for i in range(10):
+...     val = val + 2
+
+print(val)
+
+#The value of val increments by 2 ten times and increases from 2 to 22.
+
+#to exit interactive program use command below:
+exit()
